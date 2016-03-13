d9be6e0a655b729d4b390d5bf2d2d77fd6510126
xwdoor <xwdoor@126.com>
2016-03-13 21:14:25 +0800

    Revert "Revert "模拟登录""
    This reverts commit a4a2917760ad836bc00d55eac755502c9db2866b.

--- app/src/main/java/net/xwdoor/roommate/engine/RemoteService.java
+++ app/src/main/java/net/xwdoor/roommate/engine/RemoteService.java
@@ -1,19 +1,30 @@
 package net.xwdoor.roommate.engine;
 
+import com.google.gson.Gson;
+
+import net.xwdoor.roommate.mockdata.MockLoginSuccess;
+import net.xwdoor.roommate.mockdata.MockService;
+import net.xwdoor.roommate.net.RequestCallback;
+import net.xwdoor.roommate.net.Response;
+
 import okhttp3.OkHttpClient;
 
 /**
  * 远程服务器访问
- *
+ * <p>
  * Created by XWdoor on 2016/3/13.
  * 博客：http://blog.csdn.net/xwdoor
  */
 public class RemoteService {
+    public static final String API_LOGIN = "login";
+
     private static RemoteService service = null;
     private final OkHttpClient mHttpClient;
+    private final Gson gson;
 
     private RemoteService() {
         mHttpClient = new OkHttpClient();
+        gson = new Gson();
     }
 
     public static synchronized RemoteService getInstance() {
@@ -21,5 +32,17 @@ public class RemoteService {
             RemoteService.service = new RemoteService();
         }
         return RemoteService.service;
+    }
+
+    public void invoke(String apiKey, RequestCallback callback) {
+        MockService mockService = new MockLoginSuccess();
+        Response responseInJson = gson.fromJson(mockService.getJsonData(), Response.class);
+        if (callback != null) {
+            if (responseInJson.isFail()) {
+                callback.onFail(responseInJson.getError());
+            } else {
+                callback.onSuccess(responseInJson.getResult());
+            }
+        }
     }
 }
