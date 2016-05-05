3efa48ce2755dd4a61df177933a6ef3b493dedf2
git <xwdoor@126.com>
2016-05-05 13:28:26 +0800

    添加用户，刷新用户列表

--- app/src/main/java/net/xwdoor/roommate/activity/AddUserActivity.java
+++ app/src/main/java/net/xwdoor/roommate/activity/AddUserActivity.java
@@ -3,9 +3,17 @@ package net.xwdoor.roommate.activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import net.xwdoor.roommate.R;
 import net.xwdoor.roommate.base.BaseActivity;
+import net.xwdoor.roommate.engine.RemoteService;
+import net.xwdoor.roommate.net.RequestParameter;
+
+import java.util.ArrayList;
 
 /**
  * Created by XWdoor on 2016/5/4 21:57.
@@ -30,7 +38,41 @@ public class AddUserActivity extends BaseActivity {
 
     @Override
     protected void initViews(Bundle savedInstanceState) {
-        setContentView(R.layout.layout_add_user);
+        setContentView(R.layout.activity_add_user);
+        final EditText etName = (EditText) findViewById(R.id.et_name);
+        final EditText etPhone = (EditText) findViewById(R.id.et_phone);
+        final EditText etMail = (EditText) findViewById(R.id.et_mail);
+        final EditText etPassword = (EditText) findViewById(R.id.et_password);
+        Button btnAddUser = (Button) findViewById(R.id.btn_add_user);
+        btnAddUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (TextUtils.isEmpty(etName.getText()) || TextUtils.isEmpty(etPhone.getText())
+                        || TextUtils.isEmpty(etMail.getText()) || TextUtils.isEmpty(etPassword.getText())) {
+                    showToast("信息不能为空");
+                }else {
+                    addUser(etName.getText().toString(),etPhone.getText().toString(),
+                            etMail.getText().toString(),etPassword.getText().toString());
+                }
+            }
+        });
+    }
+
+    private void addUser(String name,String phone,String mail,String pwd) {
+        ArrayList<RequestParameter> params = new ArrayList<>();
+        params.add(new RequestParameter("realName", name));
+        params.add(new RequestParameter("phone", phone));
+        params.add(new RequestParameter("mail", mail));
+        params.add(new RequestParameter("password", pwd));
+
+        RemoteService.getInstance().invoke(RemoteService.API_KEY_ADD_USER, this, params, new ARequestCallback() {
+            @Override
+            public void onSuccess(String content) {
+//                Global.init(AddUserActivity.this);
+                showToast("添加成功");
+                finish();
+            }
+        });
     }
 
     @Override
--- app/src/main/java/net/xwdoor/roommate/activity/UserListActivity.java
+++ app/src/main/java/net/xwdoor/roommate/activity/UserListActivity.java
@@ -7,13 +7,19 @@ import android.view.View;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import com.google.gson.reflect.TypeToken;
+
 import net.xwdoor.roommate.R;
 import net.xwdoor.roommate.adapter.superAdapter.IViewItemBindData;
 import net.xwdoor.roommate.adapter.superAdapter.SuperAdapter;
 import net.xwdoor.roommate.adapter.superAdapter.SuperViewHolder;
 import net.xwdoor.roommate.base.BaseActivity;
 import net.xwdoor.roommate.engine.Global;
+import net.xwdoor.roommate.engine.RemoteService;
 import net.xwdoor.roommate.engine.User;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
 
 /**
  * Created by XWdoor on 2016/5/4 20:06.
@@ -40,14 +46,22 @@ public class UserListActivity extends BaseActivity {
 
     @Override
     protected void initViews(Bundle savedInstanceState) {
-        setContentView(R.layout.layout_user_list);
+        setContentView(R.layout.activity_user_list);
         TextView tvAddUser = (TextView) findViewById(R.id.tv_add_user);
+        TextView tvRefresh = (TextView) findViewById(R.id.tv_refresh);
         mLvUserList = (ListView) findViewById(R.id.lv_user_list);
 
         tvAddUser.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 AddUserActivity.startAct(UserListActivity.this);
+            }
+        });
+
+        tvRefresh.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadData();
             }
         });
 
@@ -68,6 +82,22 @@ public class UserListActivity extends BaseActivity {
 
     @Override
     protected void loadData() {
+        //初始化所有用户
+        RemoteService.getInstance().invoke(RemoteService.API_KEY_GET_ROOMMATES, this,
+                null, new ARequestCallback() {
+                    @Override
+                    public void onSuccess(String content) {
+                        Type listType = new TypeToken<ArrayList<User>>() {
+                        }.getType();
 
+                        Global.sUserList = gson.fromJson(content, listType);
+                        initAdapter();
+                    }
+
+                    @Override
+                    public void onFailure(String errorMessage) {
+                        showToast("刷新失败");
+                    }
+                });
     }
 }
